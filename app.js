bc97e9509d3b3c4455d6e5db535e375818699c71
Rithin Samuel <rithinsamuel12@gmail.com>
2023-06-13 21:40:43 +0530

    College details retrieval phase 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const College = require('./models/college');
 
 
 const app = express();
@@ -25,8 +26,23 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/mainpage', (req, res) => {
+  res.render('mainpage');
+});
+
+app.get('/collegelist', (req, res) => {
+  // res.render('collegelist');
+  College.find()
+  .then((result) => {
+    res.render('collegelist', { colleges: result });
+  })
+  .catch((err) => {
+    console.log(err);
   }
-);
+  );
+});
 
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
---
+++ models/college.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const collegeSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required:true
+    },
+    ranking: {
+        type: Number,
+        required:true
+    },
+    link : {
+        type: String,
+        required:true
+    }
+}, { timestamps: true });
+
+const College = mongoose.model('college details', collegeSchema);
+
+module.exports = College;
