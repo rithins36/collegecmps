d1f4c5ad37a30583ada1c7d53a7ad8c1973b758f
Rithin Samuel <rithinsamuel12@gmail.com>
2023-06-19 12:29:27 +0530

    Phase finalised

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
 const College = require('./models/college');
 const User = require('./models/user');
